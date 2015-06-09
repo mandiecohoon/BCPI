e6f691d478d46a3b9abe7c5f5371e2bbee918306
Dylan <dylan@dylanweb.ca>
2015-06-08 20:16:21 -0400

    Almost finished conversion to Angular with Bootstrap. Some bugs in output

--- controllers/ColorRankController.js
+++ controllers/ColorRankController.js
@@ -7,6 +7,8 @@ angular.module('ColorRank',[]).controller('ColorRankCtrl', function($scope) {
     url: "http://triplecrit.com/wp-content/themes/creativemag/images/default.png"
   };
 
+  $scope.fullColors = [];
+
   filepicker.setKey("AvKg0zzXeRNquaFMJdqfoz");
 
   // Calls FilePicker API pick function, to set Profile Picture
@@ -32,14 +34,18 @@ angular.module('ColorRank',[]).controller('ColorRankCtrl', function($scope) {
   };
 
   // Returns an array containing the colors and their frequencies
-  $scope.getColorsArray = function (imgData) {
+  $scope.getColorsArray = function () {
+    var c=document.getElementById('canvas');
+    var ctx=c.getContext('2d');
+    var imgData=ctx.getImageData(0,0,c.width,c.height);
+
     var arrayIndex = 0;
-    var fullColors = [];
+    $scope.fullColors = [];
 
     var pixel = {color:"333, 333, 333", freq:0};
-    fullColors[0] = pixel;
+    $scope.fullColors[0] = pixel;
     var pixel = {color:"333, 333, 333", freq:0};
-    fullColors[1] = pixel;
+    $scope.fullColors[1] = pixel;
 
     for (var i = 0; i < imgData.data.length; i += 4) {
       //select colors: red, green, blue, and alpha
@@ -55,9 +61,9 @@ angular.module('ColorRank',[]).controller('ColorRankCtrl', function($scope) {
       var flagColorExists = false;
 
       //check if color exists- if true then increase frequency
-      for (var j = 0; j < fullColors.length; j++) {
-        if (fullColors[j].color == color && flagColorExists == false) {
-          fullColors[j].freq = fullColors[j].freq + 1;
+      for (var j = 0; j < $scope.fullColors.length; j++) {
+        if ($scope.fullColors[j].color == color && flagColorExists == false) {
+          $scope.fullColors[j].freq = $scope.fullColors[j].freq + 1;
           flagColorExists = true;
         }
       }
@@ -67,12 +73,12 @@ angular.module('ColorRank',[]).controller('ColorRankCtrl', function($scope) {
         arrayIndex++; // add to the array index
         //create nested array of each pixel color and the frequency the color shows up
         var pixel = {color:color, freq:1};
-        fullColors[arrayIndex] = pixel;
+        $scope.fullColors[arrayIndex] = pixel;
       }
     }
 
     //call sort
-    fullColors.sort(sortByFreq);
+    $scope.fullColors.sort(sortByFreq);
 
     //sort function (greatest to least)
     function sortByFreq(a, b) {
@@ -84,92 +90,88 @@ angular.module('ColorRank',[]).controller('ColorRankCtrl', function($scope) {
       }
     }
 
-    return fullColors;
+    //return $scope.fullColors;
   }
 
-  // Coolhoon function
-  $scope.cool = function () {
-    //Get image and set its source
-    var img = new Image();
-    img.src = $scope.formImage.url;
-
-    //define array of all the pixel colors
-    var fullColors = [];
-
-    //create canvas to read pixels on the image
-    var c=document.getElementById('canvas');
-    var ctx=c.getContext('2d');
-    // Try/Catch to circumvent FireFox security error
-    try {
-      ctx.drawImage(img, 0, 0);
-
-    var imgData=ctx.getImageData(0,0,c.width,c.height);
-
-    } catch(e) {
-      if(e.name !== 'SecurityError')
-        throw e;
-      return;
-    }
-    var arrayIndex = 0; //because the for loop goes up by 4, the array needs index that counts by 1s
-
-    //initialize array with dummy pixels so that the "check if pixel color exists then add to freq"
-    var pixel = {color:"333, 333, 333", freq:0};
-    fullColors[0] = pixel;
-    var pixel = {color:"333, 333, 333", freq:0};
-    fullColors[1] = pixel;
-
-    //iterate through each pixel color(r,g,b,a)
-    for (var i=0;i<imgData.data.length;i+=4) {
-      //select colors: red, green, blue, and alpha
-      r = imgData.data[i];
-      g = imgData.data[i+1];
-      b = imgData.data[i+2];
-      a = imgData.data[i+3];
-
-      //create rgb string of the color
-      color = r + ', ' + g + ', ' + b;
-
-      //Flag if color exists stop looking & don't add it to the array
-      var flagColorExists = false;
-
-      //check if color exists- if true then increase frequency
-      for (var j = 0; j < fullColors.length; j++) {
-        if (fullColors[j].color == color && flagColorExists == false) {
-          fullColors[j].freq = fullColors[j].freq + 1;
-          flagColorExists = true;
-        }
-      }
-
-      //If color exists flag is FALSE then add the color to the array
-      if(!flagColorExists) {
-        arrayIndex++; // add to the array index
-        //create nested array of each pixel color and the frequency the color shows up
-        var pixel = {color:color, freq:1};
-        fullColors[arrayIndex] = pixel;
-      }
-
-    }
-
-    //call sort
-    fullColors.sort(sortByFreq);
-
-    //sort function (greatest to least)
-    function sortByFreq(a, b) {
-      if (a['freq'] === b['freq']) {
-        return 0;
-      }
-      else {
-        return (a['freq'] < b['freq']) ? 1 : -1;
-      }
-    }
-
-    //testing output
-    document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[0].color+');"></div>Most common color: ' + fullColors[0].color + ' with a frequency of: ' + fullColors[0].freq + '</p>');
-    document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[1].color+');"></div>Second most: ' + fullColors[1].color + ' with a frequency of: ' +fullColors[1].freq + '</p>');
-    document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[2].color+');"></div>Third most: ' + fullColors[2].color + ' with a frequency of: ' +fullColors[2].freq + '</p>');
-    document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[3].color+');"></div>Fourth most: ' + fullColors[3].color + ' with a frequency of: ' +fullColors[3].freq + '</p>');
-
-    console.log("Full Color Array: " + JSON.stringify(fullColors)); //dumping the full array in the console log
-  }
+  //// Coolhoon function
+  //$scope.cool = function () {
+  //  //Get image and set its source
+  //  var img = new Image();
+  //  // Allows image src to come from another server (FilePicker)
+  //  img.crossOrigin = "Anonymous";
+  //  img.src = $scope.formImage.url;
+  //
+  //  //define array of all the pixel colors
+  //  $scope.fullColors = [];
+  //
+  //  //create canvas to read pixels on the image
+  //  var c=document.getElementById('canvas');
+  //  var ctx=c.getContext('2d');
+  //
+  //  ctx.drawImage(img, 0, 0);
+  //
+  //  var imgData=ctx.getImageData(0,0,c.width,c.height);
+  //
+  //  var arrayIndex = 0; //because the for loop goes up by 4, the array needs index that counts by 1s
+  //
+  //  //initialize array with dummy pixels so that the "check if pixel color exists then add to freq"
+  //  var pixel = {color:"333, 333, 333", freq:0};
+  //  $scope.fullColors[0] = pixel;
+  //  var pixel = {color:"333, 333, 333", freq:0};
+  //  $scope.fullColors[1] = pixel;
+  //
+  //  //iterate through each pixel color(r,g,b,a)
+  //  for (var i=0;i<imgData.data.length;i+=4) {
+  //    //select colors: red, green, blue, and alpha
+  //    r = imgData.data[i];
+  //    g = imgData.data[i+1];
+  //    b = imgData.data[i+2];
+  //    a = imgData.data[i+3];
+  //
+  //    //create rgb string of the color
+  //    color = r + ', ' + g + ', ' + b;
+  //
+  //    //Flag if color exists stop looking & don't add it to the array
+  //    var flagColorExists = false;
+  //
+  //    //check if color exists- if true then increase frequency
+  //    for (var j = 0; j < $scope.fullColors.length; j++) {
+  //      if ($scope.fullColors[j].color == color && flagColorExists == false) {
+  //        $scope.fullColors[j].freq = $scope.fullColors[j].freq + 1;
+  //        flagColorExists = true;
+  //      }
+  //    }
+  //
+  //    //If color exists flag is FALSE then add the color to the array
+  //    if(!flagColorExists) {
+  //      arrayIndex++; // add to the array index
+  //      //create nested array of each pixel color and the frequency the color shows up
+  //      var pixel = {color:color, freq:1};
+  //      fullColors[arrayIndex] = pixel;
+  //    }
+  //
+  //  }
+  //
+  //  //call sort
+  //  $scope.fullColors.sort(sortByFreq);
+  //
+  //  //sort function (greatest to least)
+  //  function sortByFreq(a, b) {
+  //    if (a['freq'] === b['freq']) {
+  //      return 0;
+  //    }
+  //    else {
+  //      return (a['freq'] < b['freq']) ? 1 : -1;
+  //    }
+  //  }
+  //
+  //  //testing output
+  //  document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[0].color+');"></div>Most common color: ' + fullColors[0].color + ' with a frequency of: ' + fullColors[0].freq + '</p>');
+  //  document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[1].color+');"></div>Second most: ' + fullColors[1].color + ' with a frequency of: ' +fullColors[1].freq + '</p>');
+  //  document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[2].color+');"></div>Third most: ' + fullColors[2].color + ' with a frequency of: ' +fullColors[2].freq + '</p>');
+  //  document.write('<p><div style="width:10px;height:10px;border:1px solid black;background:rgb('+fullColors[3].color+');"></div>Fourth most: ' + fullColors[3].color + ' with a frequency of: ' +fullColors[3].freq + '</p>');
+  //
+  //  console.log("Full Color Array: " + JSON.stringify($scope.fullColors)); //dumping the full array in the console log
+  //}
 
 });
